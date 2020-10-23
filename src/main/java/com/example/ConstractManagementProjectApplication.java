745a705bbb7837535b6da5c4c143eb892c63bd9b
nosemasashi <ma.nose9413@gmail.com>
2020-10-23 09:53:50 +0900

    first commit

---
+++ src/main/java/com/example/ConstractManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstractManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstractManagementProjectApplication.class, args);
+	}
+
+}
